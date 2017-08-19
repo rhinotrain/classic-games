e61802606165d2605efa900afc65ab557177ce1d
rhinotrain <darchiboldster@gmail.com>
2017-08-19 17:12:31 +0100

    Drew a wicket!

--- graphics-intro/application.js
+++ graphics-intro/application.js
@@ -2,3 +2,11 @@ console.log("Hello! Welcome to your first graphics programming!")
 
 var canvas = document.getElementById("bob")
 var ctx = canvas.getContext('2d')
+
+ctx.fillStyle = 'orange'
+ctx.fillRect(305, 90, 40, 20)
+ctx.fillRect(365, 90, 40, 20)
+ctx.fillStyle = 'brown'
+ctx.fillRect(400, 100, 30, 400)
+ctx.fillRect(340, 100, 30, 400)
+ctx.fillRect(280, 100, 30, 400)
