78846026db3b50ffb1294a5feafcbea1a4a7b7e8
rhinotrain <darchiboldster@gmail.com>
2017-08-19 17:48:59 +0100

    Created wicket function

--- graphics-intro/application.js
+++ graphics-intro/application.js
@@ -3,10 +3,17 @@ console.log("Hello! Welcome to your first graphics programming!")
 var canvas = document.getElementById("bob")
 var ctx = canvas.getContext('2d')
 
-ctx.fillStyle = 'orange'
-ctx.fillRect(305, 90, 40, 20)
-ctx.fillRect(365, 90, 40, 20)
-ctx.fillStyle = 'brown'
-ctx.fillRect(400, 100, 30, 400)
-ctx.fillRect(340, 100, 30, 400)
-ctx.fillRect(280, 100, 30, 400)
+var makeWicket = function(x,y) {
+    ctx.fillStyle = 'orange'
+    ctx.fillRect(x+10, y, 25, 10)
+    ctx.fillRect(x+40, y, 25, 10)
+    ctx.fillStyle = 'brown'
+    ctx.fillRect(x+60, y+5, 15, 200)
+    ctx.fillRect(x+30, y+5, 15, 200)
+    ctx.fillRect(x, y+5, 15, 200)
+}
+
+
+
+makeWicket(50,200)
+makeWicket(600,200)
