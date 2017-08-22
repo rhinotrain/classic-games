7768061d5704465ece049b1b4dac5b6f44adc4c8
rhinotrain <darchiboldster@gmail.com>
2017-08-22 16:18:30 +0100

    Added cityskyline

--- graphics-intro/application.js
+++ graphics-intro/application.js
@@ -13,5 +13,22 @@ var ground = function() {
     ctx.fillRect(0,400,800,200)
 }
 
+var road = function() {
+    ctx.fillStyle = 'darkgrey'
+    ctx.fillRect(0,400,800,40)
+   
+}
+
+var city = function() {
+    ctx.fillStyle = 'grey'
+    ctx.fillRect(150,200,100,200)
+    ctx.fillRect(280,150,110,250)
+    ctx.fillRect(400,220,90,180)
+    ctx.fillRect(500,150,110,250)
+    ctx.fillRect(325,100,20,50)
+}
+
 skyBox()
 ground()
+road()
+city()
