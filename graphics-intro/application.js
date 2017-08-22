03e49c5e0ee4d35cc273fa91242402aad37d4355
rhinotrain <darchiboldster@gmail.com>
2017-08-22 16:51:22 +0100

    Added Basic Powerlines

--- graphics-intro/application.js
+++ graphics-intro/application.js
@@ -2,6 +2,7 @@ console.log("Hello! Welcome to your first graphics programming!")
 
 var canvas = document.getElementById("bob")
 var ctx = canvas.getContext('2d')
+var powerLinesTimer = 0
 
 var skyBox = function() {
     ctx.fillStyle = 'cyan'
@@ -16,7 +17,6 @@ var ground = function() {
 var road = function() {
     ctx.fillStyle = 'darkgrey'
     ctx.fillRect(0,400,800,40)
-   
 }
 
 var city = function() {
@@ -28,7 +28,32 @@ var city = function() {
     ctx.fillRect(325,100,20,50)
 }
 
+var powerLines = function() {
+    ctx.fillStyle = 'brown'
+    
+    ctx.fillRect(0,360,10,45)
+    ctx.fillRect(-10,365,30,5)
+    
+    ctx.fillRect(50,360,10,45)
+    ctx.fillRect(40,365,30,5)
+
+    ctx.beginPath()
+    ctx.moveTo(15, 365)
+    ctx.lineTo(25,375)
+    ctx.lineTo(35,375)
+    ctx.lineTo(45, 365)
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo(65, 365)
+    ctx.lineTo(75,375)
+    ctx.lineTo(85,375)
+    ctx.lineTo(95, 365)
+    ctx.stroke()
+}
+
 skyBox()
 ground()
 road()
 city()
+powerLines()
