e94995a38f7f4f75c438339bb6763a7de9685972
rhinotrain <darchiboldster@gmail.com>
2017-08-31 14:21:17 +0100

    Added CitySign graphic + experimetation with text

--- Field/application.js
+++ Field/application.js
@@ -1,5 +1,5 @@
 console.log ("On the road to Code City!")
-var canvas = document.getElementById("Field")
+var canvas = document.getElementById("field")
 var ctx = canvas.getContext('2d')
 
 var drawBaseBackground = function() { // background
@@ -12,11 +12,38 @@ var drawBaseBackground = function() { // background
 var drawRoad = function() {
     ctx.fillStyle = 'grey'
     ctx.fillRect(0, 400, 800, 40)
+    ctx.fillStyle = 'yellow'
+    var roadCount = 0
+    var startingPoint = 0
+
+    do {
+        ctx.fillRect (startingPoint,415,20,5)
+        startingPoint = startingPoint + 40
+        roadCount += 1
+    } while (roadCount < 21)
 }
+
+var drawCitySign = function() {
+    ctx.fillStyle = 'grey'
+    ctx.fillRect (410, 350, 7.5, 60)
+    ctx.fillRect (480, 350, 7.5, 60)
+    ctx.fillStyle = 'white'
+    ctx.fillRect (395, 295, 110, 70)
+    ctx.fillStyle = 'grey'
+    ctx.fillRect (400, 300, 100, 60)
+}
+
+var drawCitySignText = function() {
+    ctx.font = '48px serif'
+    ctx.fillText('CODE CITY', 10, 50)
+}
+
 
 var drawFrame = function() {
     drawBaseBackground()
     drawRoad()
+    drawCitySign()
+    drawCitySignText()
 }
 
 var animateNextFrame = function() {
